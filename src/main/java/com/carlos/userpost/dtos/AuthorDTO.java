29c184386724c622e548a080130a41bd1f2d1462
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-09-15 16:24:26 -0300

    Data Projection With AuthorDTO

---
+++ src/main/java/com/carlos/userpost/dtos/AuthorDTO.java
@@ -0,0 +1,36 @@
+package com.carlos.userpost.dtos;
+
+import java.io.Serializable;
+
+import com.carlos.userpost.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
