1c6afb615880ce7590f969605dca6c9042739d2a
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-09-14 23:51:52 -0300

    Create User

--- src/main/java/com/carlos/userpost/services/UserService.java
+++ src/main/java/com/carlos/userpost/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlos.userpost.domain.User;
+import com.carlos.userpost.dtos.UserDTO;
 import com.carlos.userpost.repositories.UserRepository;
 import com.carlos.userpost.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
